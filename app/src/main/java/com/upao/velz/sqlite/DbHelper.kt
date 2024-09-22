cf5deb35c4e52072cb6b23bececad3ed9b3b5122
Jean Pierre Bocanegra Valencia <jbocanegrav1@upao.edu.pe>
2024-09-22 13:16:44 -0500

    feature: Cambios Sqlite

--- app/src/main/java/com/upao/velz/sqlite/DbHelper.kt
+++ app/src/main/java/com/upao/velz/sqlite/DbHelper.kt
@@ -1,14 +1,25 @@
 package com.upao.velz.sqlite
 
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DbHelper (context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+class DbHelper(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
 
     companion object {
         val DATABASE_NAME = "velz.db"
         val DATABASE_VERSION = 1
     }
 
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(Tables().CREATE_TABLE_USER)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS " + Tables().TABLE_USER)
+        db.execSQL(Tables().CREATE_TABLE_USER)
+        onCreate(db)
+    }
 
 }
